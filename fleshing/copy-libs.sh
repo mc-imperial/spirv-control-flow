9e8caefde63f2f86af3cc3382bcae7d6bdcae970
Jack Clark <jackclark2188@gmail.com>
2022-04-01 17:05:46 +0100

    Create directory when copying across mesa libs

--- fleshing/copy-libs.sh
+++ fleshing/copy-libs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This scripts copies all needed .so files into a MESA_VERSION specific lib directory
 echo "Copying from $MESA_WORK_DIR/$MESA_VERSION-install and $MESA_WORK_DIR/drm-install into $MESA_VERSION-all-libs"
+mkdir -p $MESA_WORK_DIR/$MESA_VERSION-all-libs
 for f in `find $MESA_WORK_DIR/$MESA_VERSION-install $MESA_WORK_DIR/drm-install -name "*.so"`
 do
   cp $f $MESA_WORK_DIR/$MESA_VERSION-all-libs
