68600f42dbdb593959390a390b6614a9cfd18830
Jack Clark <jackclark2188@gmail.com>
2022-04-30 00:37:47 +0100

    Check that the glsl files from cross compilation are accepted by glslangValidator

--- fleshing/cross_compilation.py
+++ fleshing/cross_compilation.py
@@ -55,10 +55,25 @@ def cross_compile(binary_file: Path, cross_compiler: Path, cross_compiler_name: 
         assert False
 
 
-def run_cross_compilation(amber_folder: Path, spirv_as: Path, cross_compiler: Path, cross_compiler_name: str, target_lang: str):
+def validate_target_lang_output(target_lang_compiler: Path, target_lang: str, target_lang_file: Path):
+    cmd = [target_lang_compiler, target_lang_file]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    if result.returncode != 0:
+        err_string = f"ERROR: Compilation of {target_lang_file} failed!\n" \
+            f"command: {cmd}\n" \
+            f"return code: {result.returncode}\n" \
+            f"stdout:\n\n{result.stdout}\n\n" \
+            f"stderr:\n\n{result.stderr}\n\n"
+        logger.info(err_string)
+        return False
+    return True
+    
+
+def run_cross_compilation(amber_folder: Path, spirv_as: Path, cross_compiler: Path, cross_compiler_name: str, target_lang: str, target_lang_compiler: Path):
     configure_logging()
     logger.info(f"Using amber folder: {amber_folder}")
 
+    files_with_compilation_errors = []
     for amber_file in amber_utils.get_amber_files(amber_folder):
         logger.info(f"Cross compiling amber file {amber_file}")
         spirv_asm = amber_utils.extract_asm(amber_file)
@@ -68,7 +83,15 @@ def run_cross_compilation(amber_folder: Path, spirv_as: Path, cross_compiler: Pa
             f.write(spirv_asm)
         binary_file = compile_spirv(asm_file, spirv_as)
         target_lang_file = cross_compile(binary_file, cross_compiler, cross_compiler_name, target_lang)
-
+        success = validate_target_lang_output(target_lang_compiler, target_lang, target_lang_file)
+        if not success:
+            files_with_compilation_errors.append(target_lang_file)
+    
+    res_str = f"Finished cross compilation. Found {len(files_with_compilation_errors)} compilation errors."
+    if len(files_with_compilation_errors) > 0:
+        for file in files_with_compilation_errors:
+            res_str += f"\n{file}"
+    logger.info(res_str)
 
 def parse_args():
     parser = ArgumentParser()
@@ -81,7 +104,9 @@ def parse_args():
     parser.add_argument('cross_compiler_path', help='Path to the cross-compiler.', type=Path)
 
     parser.add_argument('target_lang', help="The language to cross compile to.", default="glsl", choices=["glsl", "hlsl", "msl"], type=str)
-    
+
+    parser.add_argument('target_lang_compiler_path', help="The compiler/validator to use to check the target language output.", type=Path)
+
     parser.add_argument('--cross-compiler-name', default="spirv-cross", choices=["spirv-cross"], required=False,
                         help='The compiler to use for cross compilation. Only spirv-cross is currently supported.', type=str)
     args = parser.parse_args()
@@ -112,7 +137,7 @@ def configure_logging():
 def main():
     args = parse_args()
     amber_folder = os.path.join(os.getcwd(), args.amber_folder)
-    run_cross_compilation(amber_folder, args.spirv_as_path, args.cross_compiler_path, args.cross_compiler_name, args.target_lang)
+    run_cross_compilation(amber_folder, args.spirv_as_path, args.cross_compiler_path, args.cross_compiler_name, args.target_lang, args.target_lang_compiler_path)
 
 
 if __name__ == "__main__":
