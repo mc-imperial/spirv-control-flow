da204ceef42633da8096566efd740435ae5aa5ce
Jack Clark <jackclark2188@gmail.com>
2022-05-12 01:18:36 +0100

    Add option to generate png to cfg script

--- fleshing/scripts/asm-to-cfg.sh
+++ fleshing/scripts/asm-to-cfg.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-filename=$(basename $1 .asm)
-directory=$(dirname $1)
+filename=$(basename $2 .asm)
+directory=$(dirname $2)
 full_name="$directory/$filename"
-spirv-as $1 -o "$full_name.spv" --target-env spv1.3
+spirv-as $2 -o "$full_name.spv" --target-env spv1.3 --preserve-numeric-ids
 spirv-cfg "$full_name.spv" -o "$full_name.dot"
-dot -Tpdf "$full_name.dot" -o "$full_name.pdf"
+if [ $1 == "pdf" ]; then
+  dot -Tpdf "$full_name.dot" -o "$full_name.pdf"
+else
+  dot -Tpng "$full_name.dot" -o "$full_name.png"
+fi 
+
